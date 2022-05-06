cfbaf6140b361f5e0a035fe003cc232e6f6f8563
mohanshetty <65666015+mohanshetty@users.noreply.github.com>
2022-05-06 14:43:14 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chanakyaloka</title>
+</head>
+
+<div class="content-area" id="primary">
+  <main class="site-main" id="main">
+
+    <article id="post-6" class="post-6 page type-page status-publish" itemtype="https://schema.org/CreativeWork"
+      itemscope>
+      <div class="inside-article">
+
+        <header style="text-align: center" class="entry-header" aria-label="Content">
+          <h1 class="entry-title" itemprop="headline">ಚಾಣಕ್ಯಲೋಕ &#8211; ಅಧಿಕೃತ ಪ್ರಕಟಣೆ</h1>
+          <br>
+        </header>
+
+
+        <div class="entry-content" itemprop="text">
+          <p style="text-align: left;"><strong>ದಿನಾಂಕ: <span style="color: #008000;">30 ಮಾರ್ಚ್ 2019</span> ರಂದು
+              ಶುರುವಾದ <span style="color: #008000;">ಚಾಣಕ್ಯಲೋಕ</span> ವೆಬ್ಸೈಟ್ ಸುಮಾರು 2 ವರ್ಷಗಳ ಕಾಲ
+              ಚಾಲ್ತಿಯಲ್ಲಿತ್ತು.</strong></p>
+          <hr />
+          <p style="text-align: left;"><strong>ಆದರೆ ಆಡಳಿತಾತ್ಮಕ ಕಾರಣಗಳಿಂದ ಈ ವೆಬ್ಸೈಟ್ ಅನ್ನು <span
+                style="color: #ff6600;">ಮುಂದುವರೆಸಲು ಸಾಧ್ಯವಾಗುತ್ತಿಲ್ಲ</span>.</strong></p>
+          <hr />
+          <p style="text-align: left;"><strong>ಹಾಗಾಗಿ ದಿನಾಂಕ 01 ಏಪ್ರಿಲ್ 2022 ರಿಂದ ಜಾರಿಗೆ ಬರುವಂತೆ ಚಾಣಕ್ಯಲೋಕ
+              ವೆಬ್ಸೈಟ್ ನ ಎಲ್ಲಾ ಕಾರ್ಯಗಳನ್ನು ಸ್ಥಗಿತಗೊಳಿಸಿದ್ದು, ಈ ವೆಬ್ಸೈಟ್ &#8220;<span style="color: #008000;">ಕರ್ನಾಟಕ
+                ಜಾಬ್ ಅಲರ್ಟ್</span> (<span style="color: #0000ff;"><a style="color: #0000ff;"
+                  href="https://karnatakajobalert.com/" target="_blank"
+                  rel="noopener">karnatakajobalert.com</a></span>)&#8221; ಸಂಸ್ಥೆಯೊಂದಿಗೆ ವಿಲೀನಗೊಂಡಿದೆ.</strong>
+          </p>
+          <hr />
+          <p style="text-align: left;"><strong>ಆದ ಕಾರಣ ಈ ವೆಬ್ಸೈಟ್ ನ ಎಲ್ಲಾ ಸಾಮಾಜಿಕ ತಾಣಗಳ ಖಾತೆಗಳನ್ನು ಮೇಲ್ಕಾಣಿಸಿದ
+              ಸಂಸ್ಥೆಯೇ ಮುಂದುವರೆಸುತ್ತದೆ. ಅಂದರೆ ಚಾಣಕ್ಯಲೋಕ ವಾಟ್ಸಾಪ್ ಗ್ರೂಪ್, ಟೆಲಿಗ್ರಾಮ್ ಗ್ರೂಪ್, ಫೇಸ್ಬುಕ್ ಪೇಜ್ ಹಾಗೂ
+              ಗ್ರೂಪ್, ಇನ್ಸ್ಟಾಗ್ರಾಮ್ ಪೇಜ್, ಯೂಟ್ಯೂಬ್ ಚಾನೆಲ್ ಈ ಎಲ್ಲವೂ &#8220;<span style="color: #008000;">ಕರ್ನಾಟಕ
+                ಜಾಬ್ ಅಲರ್ಟ್</span>&#8221; ಎಂಬ ಶೀರ್ಷಿಕೆಯಡಿ ಕಾರ್ಯನಿರ್ವಹಿಸಲಿದೆ.</strong></p>
+          <hr />
+          <p style="text-align: left;"><strong>2 ವರ್ಷಗಳ ಕಾಲ ಸೇವೆ ಮಾಡಲು ಅನುವುಮಾಡಿಕೊಟ್ಟ ನಿಮಗೆ ನಮ್ಮ ಅಂತಿಮ
+              ನಮನಗಳು.</strong></p>
+          <hr />
+          <p style="text-align: left;"><strong>ಇಂತಿ</strong></p>
+          <p style="text-align: left;"><strong>&#8211; <span style="color: #008000;">ಮೋಹನ್ ಶೆಟ್ಟಿ</span> ಹಾಗೂ
+              <span style="color: #ff6600;">ಚಾಣಕ್ಯಲೋಕ ಟೀಂ</span></strong></p>
+        </div>
+
+      </div>
+    </article>
+  </main>
+</div>
+
+<body>
+
+</body>
+
+</html>
